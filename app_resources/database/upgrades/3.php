036bb6f2411bf1352b9f7984e35484ab5c835262
Jacob G <jacob-x@jvvgindustries.com>
2015-06-28 17:03:54 -0400

    added new structure to database upgrade (#92)

--- app_resources/database/upgrades/3.php
+++ app_resources/database/upgrades/3.php
@@ -5,4 +5,14 @@ set_config('date_format', 'd M Y');
 set_config('time_format', 'H:i');
 echo '<li>RV3: Adding new config values... success</li>';
 
+$db->drop_field('users', 'dst');
+$new_timezone = new DBField('timezone', 'INT(3)');
+$new_timezone->set_default(0);
+$db->alter_field('users', $new_timezone);
+
+$archived_fld = new DBField('archived', 'TINYINT(1)');
+$archived_fld->add_extra('NOT NULL');
+$archived_fld->set_default(0);
+$db->add_field('forums', $archived_fld, 'num_posts');
+
 set_config('db_version', 2);
