8652bf9ec79e0cdd998793dcd196f02d0a3082d2
Jacob G <jacob-x@jvvgindustries.com>
2015-07-05 16:02:02 -0400

    upgrade utility adds missing pages and language keys (closes #108)

--- app_resources/database/upgrades/3.php
+++ app_resources/database/upgrades/3.php
@@ -54,4 +54,26 @@ foreach ($mappings as $oldtime => $newtime) {
 }
 echo '<li>RV3: Converting timezones... success</li>';
 
+ExtensionConfig::add_page('/styles', array('file' => 'style.php', 'template' => false, 'admin' => false, 'mod' => false));
+echo '<li>RV3: Adding missing pages... success</li>';
+
+ExtensionConfig::add_language_key('showallposts', 'Show all posts', 'English');
+ExtensionConfig::add_language_key('timeformatdesc', 'The following two entries allow you to set the format used for displaying all times by the software. For items that only display the date, the date format is used, but for items that display the time, the date format and time format are joined together. The formats must follow the <a href="http://php.net/manual/en/function.date.php#refsect1-function.date-parameters">PHP guidelines</a>.', 'English');
+ExtensionConfig::add_language_key('unknownerror', 'An unknown error occurred', 'English');
+ExtensionConfig::add_language_key('specificnestingerror', 'The tag <b>[$1]</b> cannot be placed directly inside <b>[$2]</b>.', 'English');
+ExtensionConfig::add_language_key('errorwaslocated', 'The above error was located at: ', 'English');
+ExtensionConfig::add_language_key('tagwasopened', 'The <b>[$1]</b> tag was opened at the following location: ', 'English');
+ExtensionConfig::add_language_key('archived', '(Archived)', 'English');
+ExtensionConfig::add_language_key('dateformat', 'Date format', 'English');
+ExtensionConfig::add_language_key('timeformat', 'Time format', 'English');
+ExtensionConfig::add_language_key('basegroupon', 'Base new group on:', 'English');
+ExtensionConfig::add_language_key('forumalreadyopen', 'You have modified a forum or already have one open for editing. Please refresh the page and then try again.', 'English');
+ExtensionConfig::add_language_key('existingwords', 'Existing words', 'English');
+ExtensionConfig::add_language_key('bbcodeinPM', 'BBCode in private messages', 'English');
+ExtensionConfig::add_language_key('otherforumeditsconfirmrefresh', 'Edits to a forum have been submitted in another window. Do you want to refresh this page to reflect those changes?', 'English');
+ExtensionConfig::add_language_key('archiveforum', 'Archive forum', 'English');
+ExtensionConfig::add_language_key('changecategory', 'Change category', 'English');
+ExtensionConfig::add_language_key('noextdir', 'The directory "app_config/extensions" does not exist or is not writable. Please create it and change the file permissions appropriately to fix this (if in doubt, chmod to 0777).', 'English');
+echo '<li>RV3: Adding missing language keys... success</li>';
+
 set_config('db_version', 3);
