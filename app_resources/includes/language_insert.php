d2409022d59fd47f957bf99c1608572aa91df409
Jacob G <jacob-x@jvvgindustries.com>
2015-07-08 14:44:13 -0400

    fixed base url in language stuff

--- app_resources/includes/language_insert.php
+++ app_resources/includes/language_insert.php
@@ -5,6 +5,7 @@ if (file_exists(FORUM_ROOT . '/app_config/cache/install/language/' . $page . '.p
 	include FORUM_ROOT . '/app_config/cache/install/language/' . $page . '.php';
 	redirect('install.php?language_insert=' . ($page + 1));
 } else {
+	$base_config = array('baseurl' => get_cookie_data('baseurl'));
 	CacheEngine::CacheLanguage();
 	?>
 	<form action="install.php" method="post" enctype="multipart/form-data">
